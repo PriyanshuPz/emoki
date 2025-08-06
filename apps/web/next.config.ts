355a81c66bfaee1508f03baa65b2a545f7500e9b
Priyanshu Verma <priyanshuverma@outlook.in>
2025-08-06 20:53:20 +0530

    chore: update start script in package.json and remove rewrites from next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,16 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  rewrites: async () => {
-    const serverUrl =
-      process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3001";
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: `${serverUrl}/api/auth/:path*`, // Proxy to Backend
-      },
-    ];
-  },
 };
 
 export default nextConfig;
