d390ae1dbd8e57d03c41ad75c2eb3ed70979b217
Priyanshu Verma <priyanshuverma@outlook.in>
2025-08-06 20:50:40 +0530

    refactor: improve code formatting and structure in index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,27 +10,37 @@ import { logger } from "hono/logger";
 const app = new Hono();
 
 app.use(logger());
-app.use("/*", cors({
-  origin: process.env.CORS_ORIGIN || "",
-  allowMethods: ["GET", "POST", "OPTIONS"],
-  allowHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
+app.use(
+  "/*",
+  cors({
+    origin: process.env.CORS_ORIGIN || "",
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
-
-app.use("/trpc/*", trpcServer({
-  router: appRouter,
-  createContext: (_opts, context) => {
-    return createContext({ context });
-  },
-}));
-
-
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  })
+);
 
 app.get("/", (c) => {
-  return c.text("OK");
+  const clientURL = process.env.CORS_ORIGIN || "http://localhost:3001";
+  // redirect to the client URL
+  if (c.req.raw.url === "/") {
+    return c.redirect(clientURL);
+  }
+
+  // otherwise, return a simple message
+  return c.text("Welcome to the Emoki API server!");
 });
 
 export default app;
